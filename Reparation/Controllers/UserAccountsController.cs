6a5d226ad3b94dfbec7ba0d9b40d4646f3c4e485
Rahul Singh <mr.rahulsingh@hotmail.com>
2019-09-03 15:19:03 +0200

    updated Rights and permissions

--- Reparation/Controllers/UserAccountsController.cs
+++ Reparation/Controllers/UserAccountsController.cs
@@ -66,7 +66,7 @@ namespace Reparation.Controllers
                         db.SaveChanges();
                     }
                     ModelState.Clear();
-                    ViewBag.Message = account.FullName + "har registrerats";
+                    ViewBag.Message = account.FullName + " har registrerats";
 
                 }
 
